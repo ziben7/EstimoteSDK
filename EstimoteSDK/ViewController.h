04f873f775ef3ff5d47ba4109d2318496ba5be7c
ziben7 <mac@macs-MacBook-Pro.local>
2015-02-22 03:01:31 +0400

    Initial Commit

---
+++ EstimoteSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EstimoteSDK
+//
+//  Created by mac on 2/22/15.
+//  Copyright (c) 2015 ziben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
